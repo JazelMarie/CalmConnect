904a0aa2aff37519e09050f38f8dba99c752ad72
jazelmarie <jazelmarie.dogomeo@wvsu.edu.ph>
2024-12-10 13:21:27 +0800

    App.js

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SchedulerComponent from './components/SchedulerComponent'; // Adjust the path as necessary
 import Home from './pages/Home';
 import Chat from './pages/Chat';
 import Appointment from './pages/Appointment';
@@ -8,6 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/scheduler" element={<SchedulerComponent />} />
         <Route path="/chat" element={<Chat />} />
         <Route path="/appointments" element={<Appointment />} />
       </Routes>
